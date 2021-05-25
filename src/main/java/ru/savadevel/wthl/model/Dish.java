d9403a788b5a41f4169b95dc150a8168c848ccbb
Savadevel <savadevel@gmail.com>
2021-05-25 18:04:45 +0300

    chore: remove from dictionaries links to external entity

--- src/main/java/ru/savadevel/wthl/model/Dish.java
+++ src/main/java/ru/savadevel/wthl/model/Dish.java
@@ -1,25 +1,16 @@
 package ru.savadevel.wthl.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
-@Getter
-@ToString(callSuper = true, exclude = "menus")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "dishes", uniqueConstraints = {@UniqueConstraint(columnNames = "name", name = "dishes_unique_name_idx")})
 public class Dish extends AbstractNamedEntity {
-
-    @OneToMany(mappedBy = "dish", fetch = FetchType.LAZY)
-    @JsonBackReference("dish<menu")
-    private List<Menu> menus;
-
     public Dish(Integer id, String name) {
         super(id, name);
     }
--- src/main/java/ru/savadevel/wthl/model/Restaurant.java
+++ src/main/java/ru/savadevel/wthl/model/Restaurant.java
@@ -1,29 +1,16 @@
 package ru.savadevel.wthl.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
-@Getter
-@ToString(callSuper = true, exclude = {"menus", "votes"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "restaurants", uniqueConstraints = {@UniqueConstraint(columnNames = "name", name = "restaurants_unique_name_idx")})
 public class Restaurant extends AbstractNamedEntity {
-
-    @OneToMany(mappedBy = "restaurant", fetch = FetchType.LAZY)
-    @JsonBackReference("restaurant<menu")
-    private List<Menu> menus;
-
-    @OneToMany(mappedBy = "restaurant", fetch = FetchType.LAZY)
-    @JsonBackReference("restaurant<vote")
-    private List<Vote> votes;
-
     public Restaurant(Integer id, String name) {
         super(id, name);
     }
